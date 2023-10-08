7e7426fa185d959a4076fe28ffd5b5daaee40c24
Jean <63329864+RNXMAJOR@users.noreply.github.com>
2023-10-08 00:32:30 -0500

    Add files via upload

--- PROJECT1/webapp.py
+++ PROJECT1/webapp.py
@@ -1,65 +1,60 @@
 #!/usr/bin/env python3
-'''
-here are the library we will be using for the deplyement of teh model
-'''
-from flask import Flask, render_template, request, jsonify
-import os
-from PIL import Image
+from flask import Flask, render_template, request
 import numpy as np
-import pickle
+from PIL import Image
+import tensorflow as tf
 
 app = Flask(__name__)
 
-# Load pickle model 
-# Load the pre-trained model
-with open('/models/model.pkl', 'rb') as model_file:
-	loaded_model = pickle.load(model_file)
+# Define the path to your SavedModel directory
+saved_model_path = "my_model"
+
+# Load the SavedModel and specify input/output names
+model = tf.saved_model.load(saved_model_path)
 
 # Define a function to preprocess the image
 def preprocess_image(image):
-	# Resize the image to match the input size of the model (e.g., 224x224 for many image classification models)
-	image = image.resize((224, 224))
-	
-	# Convert the image to a NumPy array
-	image_array = np.array(image)
-	
-	# Preprocess the image data (you may need to adapt this preprocessing based on your model)
-	image_array = image_array / 255.0  # Normalize pixel values to [0, 1]
-	image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
-	return image_array
+    image = image.resize((224, 224))
+    image_array = np.array(image, dtype=np.float32) / 255.0
+    image_array = np.expand_dims(image_array, axis=0)
+    return image_array
 
+# Define a function to check if the file extension is allowed
 def allowed_file(filename):
-	# Define a list of allowed image extensions
-	allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
+# Define routes
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction = None
 
-'''
-Load the first page first
-'''
-@app.route("/", methods=["GET"])
-def Home():
-	return render_template('index.html')
+    if request.method == "POST":
+        uploaded_file = request.files.get("imagefile")
 
-'''
-Predict the picture inserted
-'''
-@app.route("/", methods=['POST'])
-def predict():
-	uploaded_file = request.files.get("imagefile")
-	if uploaded_file and allowed_file(uploaded_file.filename):
-		# Open and preprocess the image using PIL
-		image = Image.open(uploaded_file)
-		preprocessed_image = preprocess_image(image)
-		# Make a prediction using the loaded model
-		predictions = model.predict(preprocess_image)
-		prediction = f'The leaf prediction is {predictions}'
-	else:
-		prediction = "ERROR! file"
-		
-	return render_template("index.html", prediction= prediction)
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            try:
+                # Open and preprocess the image using PIL
+                image = Image.open(uploaded_file)
+                preprocessed_image = preprocess_image(image)
 
+                # Make predictions
+                predictions = model.signatures["serving_default"](tf.constant(preprocessed_image, dtype=tf.float32))
+                predicted_probabilities = predictions['dense_2'][0]  # Extract the probability values
+                predicted_class_index = np.argmax(predicted_probabilities)  # Find the index with the highest probability
+                predicted_class_probability = predicted_probabilities[predicted_class_index]  # Get the probability of the predicted class
+
+                # Calculate the percentage (multiply by 100)
+                predicted_class_percentage = predicted_class_probability * 100
+
+                # Provide the prediction as a string
+                prediction = f'The leaf prediction is class {predicted_class_index} at {predicted_class_percentage:.2f}%'
+            except Exception as e:
+                prediction = f'Error: {str(e)}'
+        else:
+            prediction = "ERROR! Invalid file format."
+
+    return render_template("index.html", prediction=prediction)
 
 if __name__ == '__main__':
-	app.run(port=3000, debug=True)
-	
+    app.run(port=3000, debug=True)
